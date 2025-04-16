2fd79b7efa2c2d9319f1c23a5ae637aed7476f6e
NataliaGumarova <n.gu.47943@gmail.com>
2025-04-16 20:18:08 +0400

    Revert "task 7"

--- src/Task7.java
+++
@@ -1,9 +0,0 @@
-public class Task7 {
-    public static void main(String[] args) {
-        System.out.println("Задание № 7");
-        int m = 100000;
-        final int P = 12;
-        int s = (m * P / 100) * 2 + m;
-        System.out.println("Сумма вклада через 2 года = " + s);
-    }
-}
